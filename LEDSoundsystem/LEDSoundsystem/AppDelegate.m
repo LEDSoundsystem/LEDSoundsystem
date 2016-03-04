06d7168e232346de278861971abaa2f85e1981c4
cjurden <colejurden@gmail.com>
2016-03-04 15:47:10 -0600

    not sure what to put for Callback URL

--- LEDSoundsystem/LEDSoundsystem/AppDelegate.m
+++ LEDSoundsystem/LEDSoundsystem/AppDelegate.m
@@ -5,11 +5,12 @@
 //  Created by Nicholas Jurden on 3/3/16.
 //  Copyright © 2016 EECS 700 Group. All rights reserved.
 //
-
+#import <Spotify/Spotify.h>
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
-
+@property (nonatomic, strong) SPTSession *session;
+@property (nonatomic, strong) SPTAudioStreamingController *player;
 @end
 
 @implementation AppDelegate
@@ -17,8 +18,82 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    //Spotify Authorization Logic
+    [[SPTAuth defaultInstance] setClientID:@"e3962c9920404df09de0277ad66a44aa"];
+    
+    //****************************************
+    //  NEED TO DEFINE CALLBACK URL
+    //****************************************
+    [[SPTAuth defaultInstance] setRedirectURL:[NSURL URLWithString:@"callback"]];
+    [[SPTAuth defaultInstance] setRequestedScopes:@[SPTAuthStreamingScope]]; //this is where you define the scope of permission requirement
+    
+    // Construct a login URL and open it
+    NSURL *loginURL = [[SPTAuth defaultInstance] loginURL];
+    
+    // Opening a URL in Safari close to application launch may trigger
+    // an iOS bug, so we wait a bit before doing so.
+    [application performSelector:@selector(openURL:)
+                      withObject:loginURL afterDelay:0.1];
+    //End Spotify Authorization Logic
+    
     return YES;
 }
+
+
+//***************************************************************************************************
+//Spotify Auth Methods
+//***************************************************************************************************
+
+// Handle auth callback
+-(BOOL)application:(UIApplication *)application
+           openURL:(NSURL *)url
+ sourceApplication:(NSString *)sourceApplication
+        annotation:(id)annotation {
+    
+    // Ask SPTAuth if the URL given is a Spotify authentication callback
+    if ([[SPTAuth defaultInstance] canHandleURL:url]) {
+        [[SPTAuth defaultInstance] handleAuthCallbackWithTriggeredAuthURL:url callback:^(NSError *error, SPTSession *session) {
+            
+            if (error != nil) {
+                NSLog(@"*** Auth error: %@", error);
+                return;
+            }
+            
+            // Call the -playUsingSession: method to play a track
+            [self playUsingSession:session];
+        }];
+        return YES;
+    }
+    
+    return NO;
+}
+
+-(void)playUsingSession:(SPTSession *)session {
+    
+    // Create a new player if needed
+    if (self.player == nil) {
+        self.player = [[SPTAudioStreamingController alloc] initWithClientId:[SPTAuth defaultInstance].clientID];
+    }
+    
+    [self.player loginWithSession:session callback:^(NSError *error) {
+        if (error != nil) {
+            NSLog(@"*** Logging in got error: %@", error);
+            return;
+        }
+        
+        NSURL *trackURI = [NSURL URLWithString:@"spotify:track:58s6EuEYJdlb0kO7awm3Vp"];
+        [self.player playURIs:@[ trackURI ] fromIndex:0 callback:^(NSError *error) {
+            if (error != nil) {
+                NSLog(@"*** Starting playback got error: %@", error);
+                return;
+            }
+        }];
+    }];
+}
+//****************************************************************************************
+//end spotify auth methods
+//****************************************************************************************
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -41,5 +116,9 @@
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
+
+
+
+
 
 @end
